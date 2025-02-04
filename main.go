226a5c61ec036fa6ff1be8f06286f7d64ddaef6b
Mouhamadou moustapha diagne <mousdef34@gmail.com>
2025-02-04 10:34:08 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+)
+
+const (
+	header = `<!DOCTYPE html>
+	<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Markdown Preview Tool</title>
+	</head>
+	<body>
+	`
+	footer = `
+	</body>
+	</html>
+	`
+)
+
+func main() {
+	filename := flag.String("file", "", "Markdown file to preview")
+
+	flag.Parse()
+
+	if *filename == "" {
+		flag.Usage()
+
+		os.Exit(1)
+	}
+
+	if err := run(*filename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
+
+func run(filename string) error {
+	input, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+
+	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
+
+	return saveHtml(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	// Create a buffer of bytes to write to file
+	var buffer bytes.Buffer
+
+	// Write html to bytes buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHtml(outName string, data []byte) error {
+	// write the bytes to the file
+	return ioutil.WriteFile(outName, data, 0644)
+}
